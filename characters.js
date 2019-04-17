a3e45d9ae64a53525ba1a97c58a910abdd51563a
rakshyakc <rakshya.khatri1@gmail.com>
2019-04-17 14:04:06 -0400

    Included actor name in the character page

--- characters.js
+++ characters.js
@@ -1,5 +1,7 @@
 const TMDB = window.theMovieDb
+// console.log('TMDB: ', TMDB)
 const id = parseInt(window.location.search.replace('?id=', ''))
+// console.log('actor id: ', id)
 
 const byPopularity = (a, b) => a.popularity < b.popularity ? 1 : -1
 const hasPoster = x => x.poster_path !== null
@@ -7,12 +9,11 @@ const hasPoster = x => x.poster_path !== null
 function addCharacters(response) {
   const characters = JSON.parse(response).cast.sort(byPopularity).filter(hasPoster)
   const $characters = document.querySelector('.characters')
-
   characters.forEach((character) => {
     if (character.profile_path === null) return
 
     const $link = document.createElement('a')
-    $link.href = 'character.html?name=' + character.character + '&title=' + (character.title || character.name)
+    $link.href = 'character.html?name=' + character.character + '&title=' + (character.title || character.name) + '&actorId=' + id
     $link.className = 'character'
 
     const $img = document.createElement('img')
@@ -23,4 +24,11 @@ function addCharacters(response) {
   })
 }
 
+function getActorName(response) {
+  // console.log('getActorName', response)
+  const actor = JSON.parse(response).name
+  console.log('Actor: ', actor)
+}
+
 TMDB.people.getCredits({ id: id }, addCharacters, console.error)
+TMDB.people.getById({ id: id }, getActorName, console.error)
---
+++ store.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const store = {
+}
+// store.game = game
+
+module.exports = store
