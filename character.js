74e19399ac99b9e10b054e08df3d563883af3666
rakshyakc <rakshya.khatri1@gmail.com>
2019-04-17 14:04:06 -0400

    Included actor name in the character page

--- character.js
+++ character.js
@@ -1,16 +1,28 @@
+const TMDB = window.theMovieDb
+
 const name = decodeURI(window.location.search.match(/name=([^&]+)/)[1])
 const title = decodeURI(window.location.search.match(/title=([^&]+)/)[1])
+const actorId = decodeURI(window.location.search.match(/actorId=([^&]+)/)[1])
 
-const fullTitle =
-  "That's "
-  + '<strong>'
-  + name
-  + '</strong>'
-  +' from '
-  + '<strong>'
-  + title
-  + '</strong>'
-  + '. Nice.'
+function getActorName(response) {
+  const actor = JSON.parse(response).name
+  const fullTitle =
+    "That's "
+    + '<strong>'
+    + name
+    + '</strong>'
+    + ' from '
+    + '<strong>'
+    + title
+    + '</strong>'
+    + ' played by '
+    + '<strong>'
+    + actor
+    + '</strong>'
+    + '. Nice.'
 
-const $title = document.querySelector('.title')
-$title.innerHTML = fullTitle
+    const $title = document.querySelector('.title')
+    $title.innerHTML = fullTitle
+}
+
+TMDB.people.getById({ id: actorId }, getActorName, console.error)
